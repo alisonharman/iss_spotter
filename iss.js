61eb6cc642ec9d74055f68c2eca01c9c9afdc397
Alison Harman <alison_harman@sympatico.ca>
2021-11-23 03:22:20 +0000

    created function to find latitude and longitude based on IP address

--- iss.js
+++ iss.js
@@ -23,10 +23,31 @@ const fetchMyIP = function(callback) {
     }
     const ipAddress = JSON.parse(body)["ip"];
     callback(null, ipAddress);
+    return ipAddress;
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+ // const data = { latitude: '49.27670', longitude: '-123.13000' }
+ request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+
+  if (error) {
+    callback(error, null);
+    return;
+  }
+
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+  const { latitude, longitude } = JSON.parse(body);
+ callback(null, { latitude, longitude });
+ })
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
 /*
 request('http://www.google.com', (error, response, body) => {
